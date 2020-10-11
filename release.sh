2233149a5998e1e915f041c382709432aff4d665
Alexandre Rademaker <arademaker@gmail.com>
2020-10-11 16:42:54 -0300

    prerelease

--- release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sbcl --eval "(ql:quickload :glosstag)" --eval "(in-package :glosstag)" --eval '(process-corpus "~/work/wn/glosstag/data/*.plist" "ar.data" "query.csv")' --eval '(sb-ext:exit)'
